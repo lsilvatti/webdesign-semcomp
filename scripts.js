5e6685c4e79fffaac2a9cd85ce1ef6450ce1d686
Leonardo Silvatti <leosilvatti0@gmail.com>
2020-09-29 16:52:24 -0300

    colors!

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function easterEgg(){
+    $(".hello-world").toggleClass("easter-hero");
+    $(".text-hero").toggleClass("easter-text");
+    $(".go-back").toggleClass("visible");
+    console.log("worked!");
+}
